d388734a8be369eb9d409c8a409ff4e627ab49be
Jonah K. Menson <125918196+JKMenson@users.noreply.github.com>
2023-05-10 15:18:18 -0700

    Add files via upload

---
+++ peer1.py
@@ -0,0 +1,38 @@
+import socket
+import sqlite3
+import datetime
+
+dbCon = sqlite3.connect('databaseDB')
+cursor = dbCon.cursor()
+
+msgTable = ''' CREATE TABLE IF NOT EXISTS databaseTable(msgID INTEGER PRIMARY KEY, message TEXT NOT NULL, host TEXT NOT NULL, time_sent TEXT NOT NULL);'''
+cursor.execute(msgTable)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+hostname = socket.gethostname()
+host = socket.gethostbyname(hostname)
+port = 3000
+
+sock.bind((host, port))
+while True:
+    print("Ready to send data")
+    message = input("Write your message: ").encode("utf-8")
+    time_sent = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    sock.sendto(message, (host, 3001))
+    print(f"{message}, {(host, port)}, {time_sent}",)
+    
+    print("Ready to receive: ")
+    data, addressInfo = sock.recvfrom(1024)
+    print(data.decode("utf-8") +" "+ " from "  + str(addressInfo),time_sent)
+    
+    msgData = data.decode("utf-8")
+    msgData = message
+    hostData = str(addressInfo) 
+    
+    cursor.execute("INSERT INTO databaseTable(message, host, time_sent) VALUES (?,?,?)", (msgData, hostData, time_sent))
+    dbCon.commit()
+    
+    selectQuery = ''' SELECT * FROM databaseTable'''
+    cursor.execute(selectQuery)
+    selectedMessage = cursor.fetchall()
+    print(selectedMessage)
